23c4097e54d68ba9602380ba71a616d2e0056d39
Takashi Hasuo <t.hasuo@everyleaf.com>
2013-03-20 22:59:15 +0900

    Replace deprecated code.

--- CommunityBoard/Controllers/CBPostViewController.m
+++ CommunityBoard/Controllers/CBPostViewController.m
@@ -77,7 +77,7 @@
   CGSize size = [post.text
     sizeWithFont:[UIFont fontWithName:CBFontName size:CBFontSmallSize]
     constrainedToSize:constraint
-    lineBreakMode:UILineBreakModeWordWrap];
+    lineBreakMode:NSLineBreakByWordWrapping];
  
   CGFloat height = MAX(size.height, 44.0f);
  
@@ -102,7 +102,7 @@
   CBPost *post = [self.posts objectAtIndex:[indexPath row]];
   cell.textLabel.text = post.text;
   cell.textLabel.font = [UIFont fontWithName:CBFontName size:CBFontSmallSize];
-  cell.textLabel.lineBreakMode = UILineBreakModeWordWrap;
+  cell.textLabel.lineBreakMode = NSLineBreakByWordWrapping;
   cell.textLabel.numberOfLines = 0;
   
   NSURL *url = [NSURL URLWithString:post.user.avatarURL];
